2185ca4c331f3310601d6951b8ee2856ee20bd19
mathueseses12 <matheus.oliveira.rocha12@escola.pr.gov.br>
2026-04-14 09:10:28 -0300

    Add files via upload

---
+++ blablabla.js
@@ -0,0 +1,53 @@
+// 1. exiba os numeros de 1 a 10 usando for
+for(let q = 1; q <= 10; q++){
+    console.log(q)
+}
+
+// 2. exiba os numeros de 1 a 10 usando for em ordem decrescente
+for(let q = 10; q >= 1 ; q--){
+    console.log(q)
+}
+
+// 3. exiba todos os numeros pares de 2 a 20
+for(let q = 2; q <= 20 ; q = q + 2){
+    console.log(q)
+}
+
+// 4. exiba a tabuada do 5 
+for(let q = 1; q <= 10 ; q++){
+    console.log(`5 x ${q} = ${5 * q}`)
+}
+
+// 5. calcule e exiba  a soma dos numeros de 1 ate 50 
+soma = 0
+for(let q = 0; q <= 50 ; q++){
+    soma = soma + q
+    console.log(`a soma e ${soma} `)
+}
+
+// 6. conte de 1 ate 8
+for(let q = 1; q <= 8; q++){
+    console.log(q)
+}
+
+// 7. exiba  todos os numeros impares de 1 ate 15
+for(let q = 1; q <= 15 ; q = q + 2){
+    console.log(q)
+}
+
+// 8. multiplique  os numeros de 1 a 10 por 2
+for(let q = 1; q <= 10 ; q++){
+    console.log(q + "x" + 2 + '=' + 2*q );
+}
+
+// 9. some os numeros de 5 em 5 ate 50
+for (let i = 0; i<=50; i = i + 5){
+    console.log(i);
+}
+
+// 10. calcule e exiba o quarado os numeros de 1 a 10 
+for(let f = 1; f <= 10 ; f++){
+    console.log(f + 'x' + f + "=" + f*f);
+}
+
+
